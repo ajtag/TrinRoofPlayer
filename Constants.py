bace949ea522cab4a9cec48cb0ff98fe52ea5085
ajtag <ajtag@electrictea.co.uk>
2015-09-29 18:46:19 +0100

    add area defs

--- Constants.py
+++ Constants.py
@@ -17,12 +17,15 @@ transparent = 0xff, 0xff, 0xff, 0xff
 
 bubbleroof = pygame.Rect((50, 34), (28, 33))
 island = pygame.Rect((0, 41), (12, 7))
+left_arm = pygame.Rect(16, 37, 37, 14)
 left_outer_arm = pygame.Rect((16, 42), (18, 8))
 left_inner_arm = pygame.Rect((33, 37), (16, 13))
 top_outer_arm = pygame.Rect((61, 1), (0, 18))
 top_inner_arm = pygame.Rect((60, 18), (9, 18))
+top_arm = pygame.Rect(60, 0, 12, 36)
 right_inner_arm = pygame.Rect((77, 40), (21, 12))
 right_outer_arm = pygame.Rect((97, 40), (28, 12))
+right_arm = pygame.Rect(77, 39, 51, 12)
 
 Lamp = collections.namedtuple("Lamp", ["x", "y"])
 
